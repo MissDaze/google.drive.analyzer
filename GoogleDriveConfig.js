b45dfedd739f7f13467a4a05ff90317215ca1a39
MissDaze <93678286+MissDaze@users.noreply.github.com>
2025-09-10 07:05:21 +1000

    Update GoogleDriveConfig.js

--- GoogleDriveConfig.js
+++ GoogleDriveConfig.js
@@ -1,22 +1,17 @@
 const { google } = require('googleapis');
+const config = require('./config');
 
-// Load environment variables (will be set later when running locally or in deployment)
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
-const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
-
-// Initialize OAuth2 client for Google Drive
+// Initialize OAuth2 client for Google Drive using config
 const oauth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
+  config.google.clientId,
+  config.google.clientSecret,
+  config.google.redirectUri
 );
 
 // Set refresh token if available
-if (REFRESH_TOKEN) {
+if (config.google.refreshToken) {
   oauth2Client.setCredentials({
-    refresh_token: REFRESH_TOKEN
+    refresh_token: config.google.refreshToken
   });
 }
 
